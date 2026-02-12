fffaae2400530c3a9d3ce1826e8d98dd7d31e0e8
abdelalil544-blip <abdelalil544@gmail.com>
2026-02-12 16:19:01 +0100

    using proxy for securiyt

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,5 +2,5 @@
   production: false,
   adzunaAppId: '15fb0370',
   adzunaAppKey: '4e02713ec2c3cc548b1830bae00d0d9c',
-  adzunaBaseUrl: 'https://api.adzuna.com/v1/api/jobs'
+  adzunaBaseUrl: '/api/adzuna'
 };
